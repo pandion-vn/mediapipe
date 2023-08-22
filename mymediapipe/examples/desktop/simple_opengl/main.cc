2ade98d2e0f8b3819805c948d3ab2e2f801fb603
nghiach <chnghia@gmail.com>
2023-08-22 23:12:57 +0700

    update opengl render simple demo

--- mymediapipe/examples/desktop/simple_opengl/main.cc
+++ mymediapipe/examples/desktop/simple_opengl/main.cc
@@ -68,6 +68,20 @@ int main(int argc, char** argv)
   InitializeWindowAndDevice(windowWidth, windowHeight);
   InitOpenGL(windowWidth, windowHeight);
 
+  GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+  static const GLfloat g_vertex_buffer_data[] = { 
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
   cv::Mat frame;
   while (DoEvent()) {
     if (!capture.read(frame)) {
@@ -76,6 +90,23 @@ int main(int argc, char** argv)
     }
 
     DrawFrame(frame, windowWidth, windowHeight);
+
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+      0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+      3,                  // size
+      GL_FLOAT,           // type
+      GL_FALSE,           // normalized?
+      0,                  // stride
+      (void*)0            // array buffer offset
+    );
+        
+    // Draw the triangle !
+    glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+    glDisableVertexAttribArray(0);
 
     // Ececute functions about DirectX
     // DirectXの処理
