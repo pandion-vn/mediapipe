87af21bbb2608131e6a34e9b1a0513ec4a06eb29
nghiach <chnghia@gmail.com>
2023-08-22 14:15:28 +0700

    rename simple opengl -> simple_opengl2 (integrate with mediapipe graph)

--- mymediapipe/scripts/run_simple_opengl.sh
+++ mymediapipe/scripts/run_simple_opengl.sh
@@ -1,2 +1,2 @@
-GLOG_logtostderr=1 bazel-bin/mymediapipe/examples/desktop/simple_opengl/simple_opengl \
-        --calculator_graph_config_file=myMediapipe/graphs/simple_io/simple_io_graph_gpu.pbtxt
+GLOG_logtostderr=1 bazel-bin/mymediapipe/examples/desktop/simple_opengl/simple_opengl_demo \
+        --calculator_graph_config_file=mymediapipe/graphs/simple_io/graph_gpu.pbtxt
