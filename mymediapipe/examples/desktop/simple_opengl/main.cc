d91bfedc259b96f414b029017d4d7a9c838bfbc2
nghiach <chnghia@gmail.com>
2023-08-22 11:14:54 +0700

    update opencv build

--- mymediapipe/examples/desktop/simple_opengl/main.cc
+++ mymediapipe/examples/desktop/simple_opengl/main.cc
@@ -1,5 +1,9 @@
+#include <cstdlib>
 #include "include/common.h"
 
 int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  absl::ParseCommandLine(argc, argv);
 
+  LOG(INFO) << "Initialize the camera or load the video.";
 }
