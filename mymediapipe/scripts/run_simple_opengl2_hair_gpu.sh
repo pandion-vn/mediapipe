3b1e2c97ce12e314e6561796b4ab603a59abea1b
nghiach <chnghia@gmail.com>
2023-08-29 10:19:36 +0700

    add simple opengl2 with mediapipe graph gpu support

---
+++ mymediapipe/scripts/run_simple_opengl2_hair_gpu.sh
@@ -0,0 +1,4 @@
+GLOG_logtostderr=1 bazel-bin/mymediapipe/examples/desktop/simple_opengl2/simple_opengl2_hair_seg_demo_gpu \
+    --calculator_graph_config_file=mediapipe/graphs/hair_segmentation/hair_segmentation_mobile_gpu.pbtxt
+# GLOG_logtostderr=1 bazel-bin/mymediapipe/examples/desktop/simple_opengl2/simple_opengl2_demo \
+#     --calculator_graph_config_file=mymediapipe/graphs/simple_io/graph_gpu.pbtxt
