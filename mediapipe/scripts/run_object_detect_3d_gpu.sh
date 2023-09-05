6752d0e9bf73812a67943aae3fb82462a17d98b9
nghiach <chnghia@gmail.com>
2023-09-05 07:56:30 +0700

    example deskop object detection 3d gpu

--- mediapipe/scripts/run_object_detect_3d_gpu.sh
+++ mediapipe/scripts/run_object_detect_3d_gpu.sh
@@ -1,4 +1,4 @@
 # build
-# bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADER --copt -DEGL_NO_X11 --sandbox-debug mediapipe/examples/desktop/object_detection_3d:objectron_gpu
+# bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADER --copt -DEGL_NO_X11 --sandbox_debug mediapipe/examples/desktop/object_detection_3d:objectron_gpu
 # run
 GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/object_detection_3d/objectron_gpu --calculator_graph_config_file=mediapipe/graphs/object_detection_3d/object_occlusion_tracking.pbtxt
