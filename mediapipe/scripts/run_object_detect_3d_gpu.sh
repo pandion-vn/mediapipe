aa469ac69dc759e2a165514ed4cf4338e2d653ce
nghiach <chnghia@gmail.com>
2023-09-05 15:24:34 +0700

    example deskop object detection 3d gpu

--- mediapipe/scripts/run_object_detect_3d_gpu.sh
+++ mediapipe/scripts/run_object_detect_3d_gpu.sh
@@ -4,6 +4,6 @@
 GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/object_detection_3d/objectron_gpu \
 --calculator_graph_config_file=mediapipe/graphs/object_detection_3d/objectron_desktop_gpu.pbtxt \
 --input_side_packets=box_texture_path=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/classic_colors.png,\
-box_asset_name_path=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/box.obj.uuu,\
+box_asset_name=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/box.obj.uuu,\
 obj_texture_path=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/chair/texture.jpg,\
-obj_asset_name_path=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/chair/model.obj.uuu
+obj_asset_name=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/chair/model.obj.uuu
