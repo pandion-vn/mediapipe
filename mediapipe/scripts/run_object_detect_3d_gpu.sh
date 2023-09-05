7ca5e71bdc6df5f1e3dd27f46f08a727575b909a
nghiach <chnghia@gmail.com>
2023-09-05 10:05:06 +0700

    example deskop object detection 3d gpu

--- mediapipe/scripts/run_object_detect_3d_gpu.sh
+++ mediapipe/scripts/run_object_detect_3d_gpu.sh
@@ -2,7 +2,7 @@
 # bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADER --copt -DEGL_NO_X11 --sandbox_debug mediapipe/examples/desktop/object_detection_3d:objectron_gpu
 # run
 GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/object_detection_3d/objectron_gpu \
-    --calculator_graph_config_file=mediapipe/graphs/object_detection_3d/objecttron_desktop_gpu.pbtxt \
+    --calculator_graph_config_file=mediapipe/graphs/object_detection_3d/objectron_desktop_gpu.pbtxt \
     --input_side_packets=\
         box_texture_path=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/classic_colors.png,\
         box_asset_name_path=mediapipe/examples/android/src/java/com/google/mediapipe/apps/objectdetection3d/assets/box.obj.uuu,\
