d2b180977032de023a6d76ea14d9d999a2b17977
nghiach <chnghia@gmail.com>
2023-09-05 10:03:33 +0700

    example deskop object detection 3d gpu

--- mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
+++ mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
@@ -38,6 +38,10 @@ constexpr char kWindowName[] = "MediaPipe";
 
 ABSL_FLAG(std::string, calculator_graph_config_file, "",
           "Name of file containing text format CalculatorGraphConfig proto.");
+ABSL_FLAG(std::string, input_side_packets, "",
+          "Comma-separated list of key=value pairs specifying side packets "
+          "for the CalculatorGraph. All values will be treated as the "
+          "string type even if they represent doubles, floats, etc.");
 ABSL_FLAG(std::string, input_video_path, "",
           "Full path of video to load. "
           "If not provided, attempt to use a webcam.");
@@ -50,6 +54,19 @@ absl::Status RunMPPGraph() {
   MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
       absl::GetFlag(FLAGS_calculator_graph_config_file),
       &calculator_graph_config_contents));
+
+  std::map<std::string, mediapipe::Packet> input_side_packets;
+  if (!absl::GetFlag(FLAGS_input_side_packets).empty()) {
+    std::vector<std::string> kv_pairs =
+        absl::StrSplit(absl::GetFlag(FLAGS_input_side_packets), ',');
+    for (const std::string& kv_pair : kv_pairs) {
+      std::vector<std::string> name_and_value = absl::StrSplit(kv_pair, '=');
+      RET_CHECK(name_and_value.size() == 2);
+      RET_CHECK(!mediapipe::ContainsKey(input_side_packets, name_and_value[0]));
+      input_side_packets[name_and_value[0]] =
+          mediapipe::MakePacket<std::string>(name_and_value[1]);
+    }
+  }
   LOG(INFO) << "Get calculator graph config contents: "
             << calculator_graph_config_contents;
   mediapipe::CalculatorGraphConfig config =
@@ -58,7 +75,7 @@ absl::Status RunMPPGraph() {
 
   LOG(INFO) << "Initialize the calculator graph.";
   mediapipe::CalculatorGraph graph;
-  MP_RETURN_IF_ERROR(graph.Initialize(config));
+  MP_RETURN_IF_ERROR(graph.Initialize(config, input_side_packets));
 
   LOG(INFO) << "Initialize the GPU.";
   ASSIGN_OR_RETURN(auto gpu_resources, mediapipe::GpuResources::Create());
