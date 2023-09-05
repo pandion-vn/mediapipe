aec87d69b4aa2906ecc888a29b906dcce3d5fb04
nghiach <chnghia@gmail.com>
2023-09-05 16:06:37 +0700

    example deskop object detection 3d gpu

--- mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
+++ mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
@@ -160,11 +160,11 @@ absl::Status RunMPPGraph() {
                                 .At(mediapipe::Timestamp(frame_timestamp_us))));
           
           MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
-              kInputWidthStream, mediapipe::Adopt(new std::string(absl::GetFlag(FLAGS_input_width)))
+              kInputWidthStream, mediapipe::Adopt(new std::string("640"))
                                 .At(mediapipe::Timestamp(frame_timestamp_us))));
           
           MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
-              kInputHeightStream, mediapipe::Adopt(new std::string(absl::GetFlag(FLAGS_input_height)))
+              kInputHeightStream, mediapipe::Adopt(new std::string("480"))
                                 .At(mediapipe::Timestamp(frame_timestamp_us))));
           return absl::OkStatus();
         }));
