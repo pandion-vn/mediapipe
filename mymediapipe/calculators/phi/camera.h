e7a6d6e9c694f7cbf1d52441e6f3aabfbd23d6de
nghiach <chnghia@gmail.com>
2023-11-12 22:18:45 +0700

    update opengl ik

---
+++ mymediapipe/calculators/phi/camera.h
@@ -0,0 +1,56 @@
+#ifndef CAMERA_H__
+#define CAMERA_H__
+
+class Camera
+{
+public:
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+           glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+           GLfloat yaw = -90.0f, 
+           GLfloat pitch = 0.0f) :
+            Front(glm::vec3(0.0f, 0.0f, 1.0f)),
+            MovementSpeed(5.0f),
+            MouseSensitivity(0.25f), 
+            Zoom(45.0f) {
+        this->Position = position;
+        this->WorldUp = up;
+        this->Yaw = yaw;
+        this->Pitch = pitch;
+        this->CameraType = THIRD_PERSON;
+        this->updateCameraVectors();  
+    }
+
+    // Returns the view matrix calculated using Euler Angles and the LookAt Matrix
+    glm::mat4 GetViewMatrix() {
+      return glm::lookAt(this->Position , this->Position + Front, this->WorldUp);
+    }
+
+private:  
+    glm::vec3 Target;
+    GLfloat deltaTime;
+    
+    // Calculates the front vector from the Camera's (updated) Euler Angles
+    void updateCameraVectors() {
+        // Calculate the new 
+        glm::vec3 front;
+        front.x = cos(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
+        front.y = sin(glm::radians(this->Pitch));
+        front.z = sin(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
+    
+        Direction = front;
+      
+        this->Rotation =   glm::quat(glm::vec3(0.0f,glm::radians(Yaw),glm::radians(Pitch)));
+        this->ModelRotation =  glm::quat(glm::vec3(0.0f,glm::radians(-Yaw),0.0f));
+    
+        if (CameraType==FREE_FLY)
+          this->Front = glm::normalize(front);
+    
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped 
+        //this->Front = glm::normalize(front);
+        // Also re-calculate the Right and Up vector
+        this->Right = glm::normalize(glm::cross(this->Front, this->WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        this->Up = glm::normalize(glm::cross(this->Right, this->Front));
+    }
+};
+  
+#endif
